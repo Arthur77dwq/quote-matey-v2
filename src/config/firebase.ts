95a5e6fd36fdc9bc91afe9732da0e510da4531a7
ANKESH <ankeshsharma@protonmail.com>
2026-04-09 11:38:33 +0530

    feat: animate form while entering the dom

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,24 +1,46 @@
 'use client';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { getApps,initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+export function getFirebaseApp() {
+  const isKeyAvailable =
+    process.env.NEXT_PUBLIC_FIREBASE_API_KEY &&
+    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN &&
+    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID &&
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET &&
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID &&
+    process.env.NEXT_PUBLIC_FIREBASE_APP_ID &&
+    process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  if (!isKeyAvailable) {
+    return null;
+  }
+
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
+
+  if (!getApps().length) {
+    return initializeApp(firebaseConfig);
+  }
+
+  return getApps()[0];
+}
+
+const app = getFirebaseApp();
+
+if (!app) {
+  throw Error('Unable to Initialize Firebase');
+}
+
 const auth = getAuth(app);
-
 const googleProvider = new GoogleAuthProvider();
 
 export { app, auth, googleProvider };
