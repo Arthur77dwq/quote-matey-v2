655c072f762f5df149b87d28cad4a81583e4a58f
ANKESH <ankeshsharma@protonmail.com>
2026-04-23 14:53:55 +0530

    feat: write base test cases

---
+++ tests/ChatApi.spec.tsx
@@ -0,0 +1,9 @@
+import { describe, test } from 'vitest';
+
+describe('Core Functionality', () => {
+  test('TC-01: Should extract user message.', () => {});
+  test('TC-02: Should fetch api key from environment variables.', () => {});
+  test('TC-03: Should build prompt which contains user message.', () => {});
+  test('TC-04: Should call ai model to generate quote.', () => {});
+  test('TC-05: Should provide response.', () => {});
+});
