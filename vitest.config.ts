1def1ce0ef65c2f068596349f18c54ccc85d3ee5
ANKESH <ankeshsharma@protonmail.com>
2026-04-24 13:46:32 +0530

    fix: failing tests in ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    exclude: ['e2e/**'],
+    exclude: ['node_modules', 'dist', '.next', 'e2e/**'],
     setupFiles: ['./setupTests.ts'],
     include: ['**/*.{test,spec}.ts?(x)'],
     restoreMocks: true,
