a7f7db091fa57e58ad6e58beb6172267270b74e5
ANKESH <ankeshsharma@protonmail.com>
2026-05-06 17:52:14 +0530

    fix: type error

--- src/types/subscription.d.ts
+++ src/types/subscription.d.ts
@@ -86,3 +86,24 @@ export interface Subscription {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export type SubscriptionPlan = {
+  id: string;
+  version: string;
+  name: string;
+  trend: { text: string; tranding: boolean };
+  description: string;
+  pricing: { price: string; currency: string };
+  period: string;
+  features: {
+    icon?: string;
+    text: string;
+    included: boolean;
+  }[];
+  points: {
+    text: string;
+    included: boolean;
+  }[];
+  highlighted: boolean;
+  cta: { text: string; target: string };
+};
