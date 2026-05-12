e6ab2bf7121ce1af7a21e1788fb8b56694fd2a14
ANKESH <ankeshsharma@protonmail.com>
2026-05-12 16:05:18 +0530

    refactor: lint issue

--- src/services/access.ts
+++ src/services/access.ts
@@ -1,26 +1,26 @@
-import { getPlanLimit } from '@/db/planLimit/read';
+// import { getPlanLimit } from '@/db/planLimit/read';
 import { getSubscriptionByUser } from '@/db/subscription';
-import { getUserUsage } from '@/db/usage/read';
+// import { getUserUsage } from '@/db/usage/read';
 
 export async function canUserUseFeature(params: {
   firebase_uid: string;
   type: 'text' | 'image';
 }) {
-  const sub = await getSubscriptionByUser(params.firebase_uid, 'ACTIVE');
-  if (!sub) return false;
+  await getSubscriptionByUser(params.firebase_uid, 'ACTIVE');
+  // if (!sub) return false;
 
-  const usage = await getUserUsage(params.firebase_uid, sub.plan_id);
-  const limit = await getPlanLimit(sub.plan_id);
+  // const usage = await getUserUsage(params.firebase_uid, sub.plan_id);
+  // const limit = await getPlanLimit(sub.plan_id);
 
-  if (!usage || !limit) return false;
+  // if (!usage || !limit) return false;
 
-  if (params.type === 'text') {
-    return usage.text_count < limit.text_limit;
-  }
+  // if (params.type === 'text') {
+  //   return usage.text_count < limit.text_limit;
+  // }
 
-  if (params.type === 'image') {
-    return usage.image_count < limit.image_limit;
-  }
+  // if (params.type === 'image') {
+  //   return usage.image_count < limit.image_limit;
+  // }
 
   return false;
 }
