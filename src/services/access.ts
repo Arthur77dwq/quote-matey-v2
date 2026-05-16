ce618fd117b52dfbb951677f15ba21cc82126683
ANKESH <ankeshsharma@protonmail.com>
2026-05-16 22:23:09 +0530

    fix: type issue

--- src/services/access.ts
+++ src/services/access.ts
@@ -1,26 +1,27 @@
 // import { getPlanLimit } from '@/db/planLimit/read';
-import { getSubscriptionByUser } from '@/db/subscription';
+// import { getSubscriptionByUser } from '@/db/subscription';
 // import { getUserUsage } from '@/db/usage/read';
 
 export async function canUserUseFeature(params: {
   firebase_uid: string;
   type: 'text' | 'image';
 }) {
-  await getSubscriptionByUser(params.firebase_uid, 'ACTIVE');
-  // if (!sub) return false;
+  if (params.firebase_uid)
+    // await getSubscriptionByUser(params.firebase_uid, 'ACTIVE');
+    // if (!sub) return false;
 
-  // const usage = await getUserUsage(params.firebase_uid, sub.plan_id);
-  // const limit = await getPlanLimit(sub.plan_id);
+    // const usage = await getUserUsage(params.firebase_uid, sub.plan_id);
+    // const limit = await getPlanLimit(sub.plan_id);
 
-  // if (!usage || !limit) return false;
+    // if (!usage || !limit) return false;
 
-  // if (params.type === 'text') {
-  //   return usage.text_count < limit.text_limit;
-  // }
+    // if (params.type === 'text') {
+    //   return usage.text_count < limit.text_limit;
+    // }
 
-  // if (params.type === 'image') {
-  //   return usage.image_count < limit.image_limit;
-  // }
+    // if (params.type === 'image') {
+    //   return usage.image_count < limit.image_limit;
+    // }
 
-  return false;
+    return false;
 }
