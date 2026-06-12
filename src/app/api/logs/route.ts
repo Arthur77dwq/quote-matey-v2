4c422e5cd5a33429636441d4ec15dc67aeb636e9
ANKESH <ankeshsharma@protonmail.com>
2026-06-12 15:07:18 +0530

    feat: auth active

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -1,11 +1,14 @@
+import { withAuth } from '@/lib/auth/withAuth';
 import { serverLogger as logger } from '@/lib/logger';
 
 export async function POST(req: Request) {
-  const entry = await req.json();
+  return withAuth(async () => {
+    const entry = await req.json();
 
-  await logger.error(entry.message, entry.metadata);
+    await logger.error(entry.message, entry.metadata);
 
-  return Response.json({
-    success: true,
+    return Response.json({
+      success: true,
+    });
   });
 }
