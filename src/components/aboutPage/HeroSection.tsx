0e0b3d0340ac6aa2733b0c157ed87d9928c43aec
ANKESH <ankeshsharma@protonmail.com>
2026-06-29 18:56:16 +0530

    feat: responsive hero section

--- src/components/aboutPage/HeroSection.tsx
+++ src/components/aboutPage/HeroSection.tsx
@@ -4,11 +4,11 @@ import { Badge } from '@/components/ui/badge';
 
 export default function HeroSection() {
   return (
-    <section className="relative flex justify-center items-center gap-2.5 w-full h-fit overflow-hidden">
-      <div className="relative z-0 flex items-center justify-center w-full h-fit overflow-hidden">
+    <section className="relative flex justify-center items-center gap-2.5 w-full h-fit">
+      <div className="relative z-0 flex items-center justify-center w-full h-fit">
         <Image
           src="/images/about/BackgroundSKYUNDERLAY.jpg"
-          className="w-full max-h-130.5"
+          className="w-auto h-100 sm:h-120 sm:w-auto lg:w-full lg:h-full lg:max-h-130.5"
           width={0}
           height={0}
           alt="background image"
@@ -28,14 +28,17 @@ export default function HeroSection() {
           alt="cloud2"
         />
       </div>
-      <div className="px-30 bg-linear-to-b from-white/30 via-white via-50% to-white flex items-center justify-center absolute inset-0 h-full w-full overflow-hidden">
+      <div className="lg:py-0 lg:px-30 bg-linear-to-b from-white/30 via-white via-50% to-white flex items-center justify-center absolute inset-0 h-full w-full overflow-hidden">
         <div className="flex flex-col items-center justify-center gap-2.5 w-full h-fit">
           <Badge className="rounded-full py-2.5 px-5 bg-white text-body-xs text-neutral-900 flex items-center justify center border border-neutral-100">
             About QuoteMatey
           </Badge>
-          <h1 className="w-full text-[4.68rem] font-bold tracking-tighter line-[1.2em] text-center">
-            AI-Powered Quoting Software{' '}
-            <strong className="text-warning-600">Built for Tradies</strong>
+          <h1 className="flex flex-col max-w-240 text-3xl sm:text-[3.98rem] lg:text-[4.68rem] font-bold tracking-tighter line-[1.2em] text-center">
+            <span className="text-nowrap">AI-Powered Quoting </span>
+            <span className="text-nowrap">
+              Software{' '}
+              <strong className="text-warning-600">Built for Tradies</strong>
+            </span>
           </h1>
         </div>
       </div>
