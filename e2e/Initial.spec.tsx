a0eab3899f33bb0e30d05fd13cc763462abf0f0c
ANKESH <ankeshsharma@protonmail.com>
2026-07-30 16:50:13 +0530

    refactor: e2e tests

--- e2e/Initial.spec.tsx
+++ e2e/Initial.spec.tsx
@@ -3,6 +3,6 @@ import { expect, test } from '@playwright/test';
 test('Website is running', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await expect(page).toHaveTitle(
-    'QuoteMatey - Quote Faster, Win More Jobs | AI Quoting for Tradies',
+    'AI Quote Generator for Tradies | Quote Matey',
   );
 });
