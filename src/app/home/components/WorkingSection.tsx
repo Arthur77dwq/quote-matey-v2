83ee372d07d734df74cf4734af7033bd4d89ce45
ANKESH <ankeshsharma@protonmail.com>
2026-07-30 18:05:32 +0530

    feat: responsive working section

--- src/app/home/components/WorkingSection.tsx
+++ src/app/home/components/WorkingSection.tsx
@@ -42,26 +42,29 @@ export function WorkingSection({
 
   return (
     <section className={cn('w-full flex bg-white pb-50', className)}>
-      <div className="flex w-full px-7.5 gap-17.5">
-        <div className="px-4 sm:py-5 lg:pt-14.75 sm:p-0 flex flex-col items-start justify-center gap-2.5 w-1/2 h-fit">
-          {tag && (
-            <Badge className="rounded-full py-2.5 px-5 bg-neutral-50 text-[0.87rem] font-medium font-inter text-neutral-900 flex items-center justify center border border-neutral-100">
-              {tag}
-            </Badge>
-          )}
+      <div className="flex flex-col sm:flex-row w-full px-7.5 gap-17.5">
+        <div className="px-4 sm:py-5 lg:pt-14.75 sm:p-0 flex flex-col items-start justify-center gap-17.5 w-full sm:w-1/2 h-fit">
+          <div className="flex flex-col gap-2.5">
+            {tag && (
+              <Badge className="rounded-full py-2.5 px-5 bg-neutral-50 text-[0.87rem] font-medium font-inter text-neutral-900 flex items-center justify center border border-neutral-100">
+                {tag}
+              </Badge>
+            )}
 
-          {title && (
-            <Title
-              className="text-left font-bold text-[2.125rem] sm:text-[2.75rem] lg:text-6xl!"
-              {...{ title }}
-            />
-          )}
-          {description && (
-            <Description
-              className="text-left text-neutral-600 font-inter"
-              {...{ description }}
-            />
-          )}
+            {title && (
+              <Title
+                className="w-5/6 text-left font-bold text-[2.125rem] sm:text-[2.75rem] lg:text-6xl!"
+                {...{ title }}
+              />
+            )}
+            {description && (
+              <Description
+                className="text-left text-neutral-600 font-inter"
+                {...{ description }}
+              />
+            )}
+          </div>
+
           {props.supportingText && (
             <div className="w-55 h-fit">
               <h3 className="text-[2rem] font-semibold text-[#102E60]">
@@ -73,18 +76,18 @@ export function WorkingSection({
             </div>
           )}
         </div>
-        <div className="w-1/2 h-full flex">
+        <div className="w-full sm:w-90 lg:w-1/2 h-full flex">
           <Tabs
             defaultValue={props.cards && props.cards[0].id}
             onValueChange={handleChangeValue}
             value={active ? active.id : '1'}
-            className="w-150 flex justify-center items-center gap-0"
+            className="w-150 flex justify-center items-center gap-2.5 lg:gap-0"
           >
-            <TabsList className="relative w-full h-fit flex justify-center items-end">
-              <div className="absolute -bottom-2.25 bg-white w-100 h-px" />
-              <div className="absolute top-0 bg-linear-to-b from-white to-white/0 w-79.5 h-full" />
-              <div className="rounded-br-[1.25rem] w-10 h-full border-0 border-b border-r border-neutral-100" />
-              <div className="w-79.5 flex justify-center items-center gap-2.5 p-2.5 pb-0">
+            <TabsList className="relative w-full h-fit flex justify-center items-end p-0">
+              <div className="hidden lg:block absolute -bottom-px bg-white w-100 h-px" />
+              <div className="hidden lg:block absolute top-0 bg-linear-to-b from-white to-white/0 w-79.5 h-full" />
+              <div className="hidden lg:block rounded-br-[1.25rem] w-10 h-full border-0 border-b border-r border-neutral-100" />
+              <div className="w-fit lg:w-79.5 h-fit flex justify-center items-center gap-2.5 px-2.5">
                 {props.cards?.map((card, i) => (
                   <TabsTrigger
                     key={i}
@@ -93,7 +96,7 @@ export function WorkingSection({
                   >
                     <span
                       className={cn(
-                        'w-fit h-fit rounded-full px-5 py-2.5 text-sm font-medium transition-[background-position] duration-4000',
+                        'w-fit h-fit rounded-full px-3 lg:px-5 py-1.5 lg:py-2.5 text-sm font-medium transition-[background-position] duration-4000',
                         active?.id === card.id
                           ? 'bg-[length:200%_100%] bg-[linear-gradient(to_right,#102E60_50%,#EDF1F4_50%)] bg-left text-white'
                           : 'bg-neutral-50 text-neutral-600 bg-right',
@@ -105,7 +108,7 @@ export function WorkingSection({
                 ))}
               </div>
 
-              <div className="rounded-bl-[1.25rem] w-10 h-full border-0 border-b border-l border-neutral-100" />
+              <div className="hidden lg:block rounded-bl-[1.25rem] w-10 h-full border-0 border-b border-l border-neutral-100" />
             </TabsList>
             {props.cards?.map((card, i) => (
               <TabsContent
@@ -113,8 +116,8 @@ export function WorkingSection({
                 value={card.id}
                 className="w-full flex gap-5 p-1.5 border border-neutral-100 rounded-[1.875rem]"
               >
-                <div className="flex flex-col justify-center items-center gap-10 p-10 bg-neutral-50 rounded-2xl w-full">
-                  <div className="relative rounded-[1.875rem] w-full h-81.75 overflow-hidden">
+                <div className="flex flex-col justify-center items-center gap-10 p-5 lg:p-10 bg-neutral-50 rounded-2xl w-full">
+                  <div className="relative rounded-[1.875rem] w-full h-49.5 lg:h-81.75 overflow-hidden">
                     <Image
                       src={card.image.src}
                       alt={card.image.alt}
@@ -123,7 +126,7 @@ export function WorkingSection({
                     />
                   </div>
                   <div className="flex flex-col justify-center items-center gap-1.5">
-                    <h3 className="text-2xl font-semibold text-[#102E60]">
+                    <h3 className="text-center text-2xl font-semibold text-[#102E60]">
                       {card.title}
                     </h3>
                     <p className="text-center text-body-md font-inter font-medium text-neutral-600">
